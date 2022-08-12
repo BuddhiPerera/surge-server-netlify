a3c8f79f67f760dcaeb6db87510653c80d01cb4f
sathninduk <buddhiperera27@gmail.com>
2022-08-13 00:28:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose");
+
+// port
+const port = 4000
+
+// routes
+const admin = require("./routes/admin");
+const auth = require("./routes/auth");
+const user = require("./routes/user");
+
+// config
+const Keys = require("./config/Keys");
+
+//CORS
+const cors=require("cors");
+const corsOptions ={
+    origin: Keys.CORS_URL,
+    credentials:true,
+    //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
+
+// BodyParser middleware
+const bodyParser = require("express");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(
+    Keys.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Test')
+})
+
+// Rotes
+app.use("/admin", admin);
+app.use("/auth", auth);
+app.use("/user", user);
+
+// App Listen
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
